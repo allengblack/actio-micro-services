ee6a40361d258c81556d8e9567799ea34b89768e
Gbolahan Allen <allengblack@gmail.com>
2020-02-26 23:14:48 +0100

    buggy start

---
+++ src/Actio.Api/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Actio.Common.Commands;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using System.Threading.Tasks;
+
+namespace Actio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IBusClient _busClient;
+
+        public UsersController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Post([FromBody]CreateUser command)
+        {
+            await _busClient.PublishAsync(command);
+            return Accepted();
+        }
+    }
+}
--- src/Actio.Api/Startup.cs
+++ src/Actio.Api/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Actio.Api.Handlers;
+using Actio.Common.Events;
+using Actio.Common.RabbitMq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Actio.Api
 {
@@ -26,6 +22,8 @@ namespace Actio.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddRabbitMq(Configuration);
+            services.AddSingleton<IEventHandler<ActivityCreated>, ActivityCreatedHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
